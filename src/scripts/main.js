78e71e3439c5a566d49c343a0e353af1e08b8349
Orest Vaheman <ovaheman@gmail.com>
2025-06-02 21:12:57 +0300

    add issue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,30 @@
 'use strict';
 
-const form = document.querySelector('form');
-const inputs = form.querySelectorAll('input');
+// const form = document.querySelector('form');
+const input = document.querySelectorAll('input');
+const inputs = Array.from(document.querySelectorAll('input'));
 
-for (const el of inputs) {
+function getFirstLetterBig(value) {
+  const first = value[0].toUpperCase();
+  const result = value.split('');
 
+  result[0] = first;
+
+  return result.join('');
 }
+
+inputs.forEach((element) => {
+  element.setAttribute('placeholder', getFirstLetterBig(element.name));
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  element.parentElement.insertBefore(label, element);
+
+  const idAttribute = element.getAttribute('id');
+
+  label.setAttribute('for', idAttribute);
+  console.log(idAttribute);
+
+  label.textContent = element.name;
+});
